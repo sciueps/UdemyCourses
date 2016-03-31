b52bc46043fb43bf201c18c2daecf2fe457750df
sciueps <mail@francescocioci.it>
2016-03-31 22:31:36 +0200

    StackOverFlow Post

---
+++ Stack Post/Stack Post/Post.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Stack_Post
+{
+    class Post
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Creation => DateTime.Now;
+       
+        public int CurrentVote { get; private set; }
+
+       
+
+        public Post(string title, string description)
+        {
+            this.Title = title;
+            this.Description = description;
+        }
+
+        public int UpVote(int voteCount)
+        {
+            this.CurrentVote = voteCount++;
+            return voteCount;
+        }
+
+        public int DownVote(int voteCount)
+        {
+            this.CurrentVote = voteCount--;
+            return voteCount;
+        }
+
+        public void DisplayVote(Post p)
+        {
+            var timeStamp = Creation;
+             Console.WriteLine("Post: {0} \ncreated on {1} {2}\nCurrent Vote: {3}", p.Title, timeStamp.ToShortDateString(),timeStamp.ToShortTimeString(),this.CurrentVote.ToString());
+             
+        }
+        
+
+    }
+}
---
+++ Stack Post/Stack Post/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Post
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myPost = new Post("Excercise Section 2 Lecture 15", "StackOverFlow Post");
+            myPost.UpVote(3);
+            myPost.UpVote(3);
+            myPost.DownVote(2);
+            myPost.DisplayVote(myPost);
+        }
+    }
+}
