3c07c1c18f618a894e1b377b4145ed761ea321c1
sciueps <mail@francescocioci.it>
2016-04-01 21:59:17 +0200

    Stack

---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            stack.Push(1,"prova","Oggetto X",(char)'f',"Viva Mosh");
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+        }
+    }
+}
---
+++ Stack/Stack/Stack.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Stack
+{
+    public class Stack
+    {
+
+        private readonly List<object> _stack; 
+        private object _list;
+
+        public Stack( )
+        {
+           _stack = new List<object>();
+        }
+
+        public void Push(params object[] list)
+        {
+            this._list = list;
+            if (list == null)
+            {
+                throw new InvalidCastException("Pay attention! Null value passed!");
+            }
+
+            _stack.AddRange(list);
+        }
+
+        public object Pop()
+        {
+            if (_stack.Count == 0)
+            {
+                throw new InvalidOperationException("Lo Stack è vuoto, prima dovresti aggiungere qualcosa!");
+                
+            }
+
+            var lastIndex = _stack.Count() - 1;
+            var last =  _stack.Last();
+            _stack.RemoveAt((lastIndex));
+            return last;
+        }
+
+        public void Clear()
+
+        {
+            _stack.Clear();
+        }
+
+    }
+}
